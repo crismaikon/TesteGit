fc2b1fa614416a47d44be9a640e44d5d618b8dec
Crismaikon Lins <crismaikonlins@gmail.com>
2014-05-21 16:10:59 -0300

    Loja criada cadastro de clientes implementada

---
+++ Teste/Loja.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Loja {
+    
+    private List<Cliente> clientes = new ArrayList<Cliente>();
+    
+    public void AdicionarClentes(Cliente cliente){
+        clientes.add(cliente);
+    }
+    
+}
